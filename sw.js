9a88abfded7d6e6cde3010bc6ecae4191eff645a
Herman Starikov <hermanstarikov@gmail.com>
2019-03-19 09:58:25 -0400

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-705b28779d8bec65b870.js"
+    "url": "webpack-runtime-b210a1a6161f4b76dd5d.js"
   },
   {
     "url": "app-23a5e53374ff214025ac.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "64ee203ff3403af8311ffdb236efaf6d"
+    "revision": "6cf7eb87b7c46aa616b0a6cb2d359d58"
   },
   {
     "url": "0-2390741146e31823fc6b.js"
