fb8887270b706490ed7de6d20a8389c618aee015
Herman Starikov <hermanstarikov@gmail.com>
2019-08-24 18:49:37 -0400

    cra

---
+++ src/node/index.js
@@ -0,0 +1,25 @@
+// import React from "react";
+// import ReactDOMServer from "react-dom/server";
+// import ReactMarkdown from "react-markdown";
+// import fs from "fs";
+
+const React = require("react");
+const ReactDOMServer = require("react-dom/server");
+const ReactMarkdown = require("react-markdown");
+const fs = require("fs");
+
+const input = "# This is a header\n\nAnd this is a paragraph";
+const output = ReactDOMServer.renderToString(<ReactMarkdown source={input} />);
+
+fs.mkdirSync("../components/");
+fs.mkdirSync("../components/__generated__");
+fs.writeFile(
+  "../components/__generated__/PostAboutBlah.js",
+  output,
+  "utf8",
+  (...args) => {
+    console.log(...args);
+  }
+);
+
+// React
